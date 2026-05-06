124bf32659c56ad448c7add54856fbd3533cf50d
malcolm12aa <malcolmcooldoy@gmail.com>
2026-05-06 18:15:32 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
   title: "Build Your Legend",
-  version: "0.9.3-dark-guild-interface",
+  version: "0.9.4-dnd-inspired-content",
   storageKey: "build-your-legend-save-v1",
   saveSlotCount: 5,
   maxOverallLevel: 100,
